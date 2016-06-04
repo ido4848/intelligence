8e583db96398bb3381acf82d612a7833eed45b51
ido4848 <ido.aizenbud@gmail.com>
2016-06-04 22:59:18 +0300

    added lsanomaly, which might be better then oneClassSvm

--- python_code/detect_mp3.py
+++ python_code/detect_mp3.py
@@ -3,6 +3,7 @@ from sklearn import svm
 import numpy as np
 import sys
 import pydub
+import lsanomaly
 from music_db import Segmented_Music_DB
 
 
@@ -24,15 +25,13 @@ def main():
 
     raw_song_arrays = [list(song.raw_data) for song in song_segments]
     train_data = [[ord(c) for c in raw_array] for raw_array in raw_song_arrays]
-    train_type = [1 for _ in range(len(train_data))]
 
     print "Train data is of size {}".format(len(train_data))
     # clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1) # Todo: switch back?
-    clf = svm.SVC()
+    # clf = svm.SVC()
+    clf = lsanomaly.LSAnomaly()
 
-    train_data.append([0 for _ in range(len(train_data[0]))]) # TODO: append here real sound?
-    train_type += [0]
-    clf.fit(np.array(train_data), np.array(train_type))
+    clf.fit(np.array(train_data))
 
     sample = pydub.AudioSegment.from_mp3(sample_path)
     print sample.frame_rate
@@ -41,7 +40,9 @@ def main():
     sample_cut_test = [ord(c) for c in sample_cut_array] 
     pred = clf.predict(np.array([sample_cut_test])) # TODO: better predication method
 
-    if pred[0] == 1:
+    print pred
+
+    if pred[0] == 0:
         print "Sample {} is music!".format(sample_path)
     else:
         print "Sample {} is not music!".format(sample_path)
