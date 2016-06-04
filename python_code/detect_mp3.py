304f199f072ac4e78bd46d5c57c1a358e3f48f63
ido4848 <ido.aizenbud@gmail.com>
2016-06-04 22:32:17 +0300

    switched to svc instead of oneCLassSvm (but now we have some false positives for random data)

--- python_code/detect_mp3.py
+++ python_code/detect_mp3.py
@@ -24,19 +24,22 @@ def main():
 
     raw_song_arrays = [list(song.raw_data) for song in song_segments]
     train_data = [[ord(c) for c in raw_array] for raw_array in raw_song_arrays]
+    train_type = [1 for _ in range(len(train_data))]
 
     print "Train data is of size {}".format(len(train_data))
+    # clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1) # Todo: switch back?
+    clf = svm.SVC()
 
-    clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
-    clf.fit(train_data)
+    train_data.append([0 for _ in range(len(train_data[0]))]) # TODO: append here real sound?
+    train_type += [0]
+    clf.fit(np.array(train_data), np.array(train_type))
 
     sample = pydub.AudioSegment.from_mp3(sample_path)
     print sample.frame_rate
     sample_cut = sample[0:1000 * duration_seconds:]
     sample_cut_array = list(sample_cut.raw_data)
-    sample_cut_test = [ord(c) for c in sample_cut_array]
-
-    pred = clf.predict(np.array([sample_cut_test]))
+    sample_cut_test = [ord(c) for c in sample_cut_array] 
+    pred = clf.predict(np.array([sample_cut_test])) # TODO: better predication method
 
     if pred[0] == 1:
         print "Sample {} is music!".format(sample_path)
