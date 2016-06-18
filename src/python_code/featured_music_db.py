c6bd6f428fcf0d9864eac93bc32eb490def14a5a
ido4848 <ido.aizenbud@gmail.com>
2016-06-18 12:00:32 +0300

    pkg reordering, added modules folder

---
+++ src/python_code/featured_music_db.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+import sys
+import pydub
+from pydub.playback import play
+import os
+import shelve
+import re
+import random
+
+
+class Song(object):
+    def __init__(self, file_path, file_format, sample, features):
+        self.file_path = file_path
+        self.features = features
+        self.file_format = file_format
+        self.sample = sample
+
+
+class SongAlreadyExists(Exception):
+    def __init__(self):
+        # Call the base class constructor with the parameters it needs
+        super(SongAlreadyExists, self).__init__("Song already in db!")
+
+
+class SongNotFound(Exception):
+    def __init__(self):
+        # Call the base class constructor with the parameters it needs
+        super(SongNotFound, self).__init__("Song is not in db!")
+
+
+class DBNotOpen(Exception):
+    def __init__(self, db_folder):
+        # Call the base class constructor with the parameters it needs
+        super(SongNotFound,
+              self).__init__("DB " + str(db_folder) + " is not open!")
+
+
+class SoundFormatNotSupported(Exception):
+    def __init__(self, format):
+        # Call the base class constructor with the parameters it needs
+        super(SoundFormatNotSupported, self).__init__(
+            "Sound format " + str(format) + " is not supported.")
+
+
+class FeaturedMusicDB(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def from_existing(db_folder):
+        self = FeaturedMusicDB()
+
+        self._folder = db_folder
+        self._path = self._folder + "/" + "dbfile.pkl"
+
+        self._db_instance = None
+        self.open()
+        self._channels = self._db_instance["_channels"]
+        self._frame_rate = self._db_instance["_frame_rate"]
+        self._index_counter = self._db_instance["_index_counter"]
+        self._feature_extractor = self._db_instance["_feature_extractor"]
+        self.close()
+
+        return self
+
+    @staticmethod
+    def create_new(db_folder, frame_rate, channels, feature_extractor):
+        self = FeaturedMusicDB()
+
+        self._folder = db_folder
+        self._path = self._folder + "/" + "dbfile.pkl"
+        if not os.path.exists(self._folder):
+            os.makedirs(self._folder)
+
+        self._frame_rate = frame_rate
+        self._channels = channels
+        self._index_counter = 0
+        self._feature_extractor = feature_extractor
+
+        self._db_instance = None
+        self.open()
+        self._db_instance["_channels"] = self._channels
+        self._db_instance["_frame_rate"] = self._frame_rate
+        self._db_instance["_index_counter"] = self._index_counter
+        self._db_instance["_feature_extractor"] = self._feature_extractor
+        self.close()
+        return self
+
+    def open(self):
+        return self.__enter__()
+
+    def close(self):
+        self.__exit__("", "", "")
+
+    def __enter__(self):
+        if self._db_instance is None:
+            self._db_instance = shelve.open(self._path)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self._db_instance is not None:
+            self._db_instance.close()
+            self._db_instance = None
+
+    def _open_song(self, song_file_path, file_format):
+        wav_regex = re.compile("[Ww][Aa][Vv]([Ee])?")
+        mp3_regex = re.compile("[Mm][Pp]3")
+        if wav_regex.match(file_format) is not None:
+            return pydub.AudioSegment.from_wav(song_file_path)
+        elif mp3_regex.match(file_format) is not None:
+            return pydub.AudioSegment.from_mp3(song_file_path)
+        else:
+            raise SoundFormatNotSupported(file_format)
+
+    def does_song_match(self, song):
+        return song.frame_rate == self._frame_rate and song.channels == self._channels
+
+    def add_mp3s(self, mp3_song_files):
+        for mp3_song_file in mp3_song_files:
+            try:
+                self.add_song(mp3_song_file, "mp3")
+            except Exception as e:
+                print "Error inserting mp3 song file {}: {}".format(mp3_song_file , e.args)
+
+    def add_wavs(self, wav_song_files):
+        for wav_song_file in wav_song_files:
+            try:
+                self.add_song(wav_song_file, "wav")
+            except Exception as e:
+                print "Error inserting mp3 song file {}: {}".format(wav_song_file, e.message)
+
+    def add_song(self, song_file_path, file_format):
+        if self._db_instance is None:
+            raise DBNotOpen(self._folder)
+
+        song = self._open_song(song_file_path, file_format)
+        if not self.does_song_match(song):
+            raise Exception("Song does not match db format")
+
+        features = self._feature_extractor.extract(song)
+
+        self._index_counter += 1
+
+        self._db_instance[str(self._index_counter)] = Song(song_file_path, file_format, [], features)
+        self._db_instance["_index_counter"] = self._index_counter
+
+        return self._index_counter
+
+    def remove_song(self, song_counter):
+        song_counter = str(song_counter)
+        if self._db_instance is None:
+            raise DBNotOpen(self._folder)
+
+        if song_counter not in self._db_instance:
+            raise SongNotFound()
+
+        self._db_instance[song_counter] = None
+        del self._db_instance[song_counter]
+
+    def get_song(self, song_counter):
+        song_counter = str(song_counter)
+        if self._db_instance is None:
+            raise DBNotOpen(self._folder)
+
+        if song_counter not in self._db_instance:
+            raise SongNotFound()
+
+        return self._db_instance[song_counter]
+
+    def get_all_songs(self):
+        items = self._db_instance.items()
+        songs = []
+        for item in items:
+            if type(item[1]) == type(Song("", "", "", "")):
+                songs += [item[1]]
+        return songs
+
+    def get_extractor(self):
+        return self._feature_extractor
+
+
+def create_test_db(db_folder, frame_rate, channels, duration_in_seconds, test_song_path):
+    db = FeaturedMusicDB.create_new(db_folder, frame_rate, channels, duration_in_seconds)
+    with db.open() as opened_db:
+        index = opened_db.add_song(test_song_path, "mp3")
+        print "song was inserted in index {}".format(index)
+        opened_db.remove_song(index)
+        print "Song indexed {} was removed from db".format(index)
+
+        index = opened_db.add_song(test_song_path, "mp3")
+        print "Song was inserted by index with index {}".format(index)
+        opened_db.remove_song(index)
+        print "Song indexed {} was removed from db".format(index)
+
+        index = opened_db.add_song(test_song_path, "mp3")
+        print "Song was inserted by index with index {}".format(index)
+
+
+def play_music_from_db(db_folder, count):
+    db = FeaturedMusicDB.from_existing(db_folder)
+    with db.open() as odb:
+        this_count = 0
+        songs = odb.get_all_songs()
+        while this_count < count:
+            song = random.choice(songs)
+            try:
+                play(song.sample)
+                this_count += 1
+            except:
+                pass
+
+
+FRAME_RATE = 44100
+CHANNELS = 2
+
+
+def main():
+    if len(sys.argv) == 3:
+        play_music_from_db(sys.argv[1], int(sys.argv[2]))
+
+    elif len(sys.argv) == 4:
+        create_test_db(sys.argv[1], FRAME_RATE, CHANNELS, int(sys.argv[2]), sys.argv[3], )
+
+    else:
+        print "Not a valid number of parameters"
+        return
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/python_code/music_detector.py
@@ -0,0 +1,100 @@
+import pydub
+import numpy as np
+import sys
+import sklearn
+import featured_music_db
+import shelve
+
+
+class MusicDetector(object):
+    def __init__(self, music_db_folder, regressioner):
+        self._music_db_folder = music_db_folder
+        self._music_db = featured_music_db.FeaturedMusicDB.from_existing(self._music_db_folder)
+        self._regressioner = regressioner
+        self._feature_extractor = None
+
+        self._fit()
+
+    def _fit(self):
+        songs = []
+        with self._music_db.open() as odb:
+            songs = odb.get_all_songs()
+            self._feature_extractor = odb.get_extractor()
+
+        train_data = [song.features for song in songs]
+
+        try:
+            self._regressioner.fit(np.array(train_data))
+            return
+        except Exception as e:
+            print "Error in fitting regressioner: {}".format(e.message)
+
+        try:
+            train_tags = [1 for _ in range(len(train_data))]
+            self._regressioner.fit(np.array(train_data), np.array(train_tags))
+            return
+        except Exception as e:
+            print "Error in fitting regressioner(using tags): {}".format(e.message)
+
+        try:
+            self._regressioner.fit(np.array(train_data), 1)
+        except Exception as e:
+            print "Error in fitting regressioner(using one tag): {}".format(e.message)
+
+    def detect_from_song(self, song_object):
+        with self._music_db.open() as odb:
+            if not odb.does_song_match(song_object):
+                print "Song could not be deteced (does not match db format)."
+                return
+
+        test_data = None
+        try:
+            test_data = self._feature_extractor.extract(song_object)
+        except Exception as e:
+            print "Error in extracting features from song: {}".format(e.message)
+            return
+
+        try:
+            pred = self._regressioner.predict_proba(np.array([test_data]))
+            return pred[0][0]
+        except Exception as e:
+            pass
+
+        try:
+            pred = self._regressioner.predict(np.array([test_data]))
+            return pred[0]
+        except Exception as e:
+            print "Error in predicting song: {}".format(e.message)
+
+    def detect_from_file(self, song_file_path, file_format):
+        test_song = None
+        if file_format == "mp3":
+            test_song = pydub.AudioSegment.from_mp3(song_file_path)
+        elif file_format == "wav":
+            test_song = pydub.AudioSegment.from_wav(song_file_path)
+        else:
+            raise Exception("File format not supported.")
+
+        return self.detect_from_song(test_song)
+
+
+def main():
+    if len(sys.argv) != 4:
+        print "Bad usage! usage is {} " \
+              "<music-db-folder> <test-path> <test-format>".format(sys.argv[0])
+        return
+
+    music_db_folder = sys.argv[1]
+    test_path = sys.argv[2]
+    test_format = sys.argv[3]
+
+    clf = sklearn.svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)  # Todo: switch back?
+    # clf = svm.SVC()
+    # clf = lsanomaly.LSAnomaly()
+    detector = MusicDetector(music_db_folder, clf)
+    pred = detector.detect_from_file(test_path, test_format)
+    print "pred for {} is {}".format(test_path, pred)
+
+
+if __name__ == "__main__":
+    main()
