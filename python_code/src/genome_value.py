cc98533e5d71eada0b962ed3f3cec71cf9963479
ido4848 <ido.aizenbud@gmail.com>
2017-03-16 13:33:31 +0200

    conventions

--- python_code/src/genome_value.py
+++ python_code/src/genome_value.py
@@ -3,17 +3,17 @@ import math
 
 class GenomeValue(object):
     def __init__(self, lst):
-        self.lst = lst
-        for j, a in enumerate(self.lst):
+        self._lst = lst
+        for j, a in enumerate(self._lst):
             if a < 0:
-                self.lst[j] = a - math.floor(a)
+                self._lst[j] = a - math.floor(a)
             else:
-                self.lst[j] = math.fabs(a - math.ceil(a))
-        self.index = 0
+                self._lst[j] = math.fabs(a - math.ceil(a))
+        self._index = 0
 
     def get(self, min_max_dict, is_int=True):
-        val = self.lst[self.index] * (min_max_dict['max'] - min_max_dict['min']) + min_max_dict['min']
-        self.index += 1
+        val = self._lst[self._index] * (min_max_dict['max'] - min_max_dict['min']) + min_max_dict['min']
+        self._index += 1
         if is_int:
             return int(round(val))
         return val
