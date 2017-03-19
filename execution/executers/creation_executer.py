e713f032c0e80eb9947cf2852e0f5dd91fd457c1
ido4848 <ido.aizenbud@gmail.com>
2017-03-19 19:09:17 +0200

    product_saver bug fix

--- execution/executers/creation_executer.py
+++ execution/executers/creation_executer.py
@@ -1,7 +1,7 @@
 class CreationExecuter(object):
     def __init__(self, creator, product_saver, num_of_products, verbose=True):
         self._creator = creator
-        self._product_saver = product_saver,
+        self._product_saver = product_saver
         self._num_of_products = num_of_products
 
         self._verbose = verbose
