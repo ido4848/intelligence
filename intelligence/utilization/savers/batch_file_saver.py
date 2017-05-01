a83d3b38011bbe2deedd9d0c567f5ef730c648db
ido4848 <ido.aizenbud@gmail.com>
2017-05-01 14:59:04 +0300

    utils edit

--- intelligence/utilization/savers/batch_file_saver.py
+++ intelligence/utilization/savers/batch_file_saver.py
@@ -2,17 +2,19 @@ from file_saver import FileSaver, shelve_save, pickle_save
 
 
 class BatchFileSaver(object):
-    def __init__(self, folder_path, file_path, file_saver_class=FileSaver, file_extension="", save_method=shelve_save):
+    def __init__(self, folder_path, file_path, file_saver_class=FileSaver, file_extension="", save_method=shelve_save,
+                 verbose=False):
         self._folder_path = folder_path
         self._file_path = file_path
         self._save_method = save_method
         self._file_extension = file_extension
+        self._verbose = verbose
 
         self._file_saver_class = file_saver_class
         self._index = 1
 
     def save(self, py_object):
         fs = self._file_saver_class(self._folder_path, self._file_path + "_" + str(self._index), self._file_extension,
-                                    self._save_method, verbose=False)
+                                    self._save_method, verbose=self._verbose)
         fs.save(py_object)
         self._index += 1
--- intelligence/utilization/savers/timestamp_file_saver.py
+++ intelligence/utilization/savers/timestamp_file_saver.py
@@ -5,14 +5,15 @@ from file_saver import FileSaver, shelve_save, pickle_save
 
 
 class TimestampFileSaver(object):
-    def __init__(self, folder_path, file_path, file_extension="", save_method=shelve_save):
+    def __init__(self, folder_path, file_path, file_extension="", save_method=shelve_save, verbose=True):
         self._folder_path = folder_path
         self._file_path = file_path
         self._save_method = save_method
         self._file_extension = file_extension
+        self._verbose = verbose
 
     def save(self, py_object):
         time_path = datetime.datetime.now().strftime("%y_%m_%d/%H_%M_%S")
         fs = FileSaver(os.path.join(self._folder_path, time_path), self._file_path, self._file_extension,
-                       self._save_method)
+                       self._save_method, verbose=self._verbose)
         return fs.save(py_object)
